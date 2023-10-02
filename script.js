40442227f535b7766f14ec35428b62eafd6aa947
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-10-02 16:12:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,32 +30,41 @@ function crearCeldas() {
     }
 }
 let generados = [];
+let mostrables = [];
+
 function generarValor() {
     let min = Math.ceil(1);
     let max = Math.floor(76);
     let letras = ["B", "I", "N", "G", "O"];
     while (true) {
-    let valor = Math.floor(Math.random() * (max - min) + min);
-    if (!generados.includes(valor)) {
-        let cell = document.getElementById(`n${valor}`);
-        cell.style.backgroundColor = "green";
-        generados.push(valor);
-        if (valor < 16) {
-            return `${letras[0]}${valor}`;
-        }else if (valor >= 16 && valor <= 30) {
-            return `${letras[1]}${valor}`;
-        }else if (valor >= 31 && valor <= 45) {
-            return `${letras[2]}${valor}`;
-        }else if (valor >= 46 && valor <= 60) {
-            return `${letras[3]}${valor}`;
-        }else if (valor >= 61 && valor <= 75) {
-            return `${letras[4]}${valor}`;
+        let valor = Math.floor(Math.random() * (max - min) + min);
+        if (!generados.includes(valor)) {
+            let cell = document.getElementById(`n${valor}`);
+            cell.style.backgroundColor = "green";
+            generados.push(valor);
+            if (mostrables.length >= 5) {
+                mostrables.shift();
+            }
+            mostrables.push(designarLetra(letras, valor));
+            return designarLetra(letras, valor);
+        }
+        if (generados.length === 75) {
+            alert("Ya se han jugado todos los numeros");
+            return;
         }
     }
-    if (generados.length === 75) {
-        alert("Ya se han jugado todos los numeros");
-        return;
-    }
+}
+function designarLetra(letras, valor) {
+    if (valor < 16) {
+        return `${letras[0]}${valor}`;
+    } else if (valor >= 16 && valor <= 30) {
+        return `${letras[1]}${valor}`;
+    } else if (valor >= 31 && valor <= 45) {
+        return `${letras[2]}${valor}`;
+    } else if (valor >= 46 && valor <= 60) {
+        return `${letras[3]}${valor}`;
+    } else if (valor >= 61 && valor <= 75) {
+        return `${letras[4]}${valor}`;
     }
 }
 function mostrarValor() {
@@ -63,16 +72,30 @@ function mostrarValor() {
     texto.textContent = generarValor();
 }
 let interval;
-function autoGen(){
-    if(!interval){
+function autoGen() {
+    if (!interval) {
         interval = setInterval(() => {
-            if(generados.length === 75){
-                clearInterval(interval)
-                interval = null
+            if (generados.length === 75) {
+                clearInterval(interval);
+                interval = null;
             }
             mostrarValor();
-        },)
+        }, 1000);
     }
-    
+}
+function cancelGen() {
+    clearInterval(interval);
+    interval = null;
+    value = false;
+}
+function resetBingo(){
+    for(let x = 0; x < generados.length; x++){
+        let cell = document.getElementById(`n${generados[x]}`);
+        cell.style.backgroundColor = "transparent";
+    }
+    let valor = document.getElementById("valorGenerado");
+    valor.textContent = "";
+    generados = [];
+    mostrables = [];
 }
 window.onload = crearCeldas;
